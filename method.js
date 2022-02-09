485db6e1b6c4e3c53e12b54355dcf8d42c36c47c
FerLanzaC <88668300+FerLanzaC@users.noreply.github.com>
2022-02-09 10:57:07 -0600

    Create method.js

---
+++ method.js
@@ -0,0 +1,14 @@
+async function sayJoke(apiUrl, jokeId){
+  const response = await fetch (apiUrl)
+  const json = await response.json()
+  for(let joke of json.jokes){
+      if (joke.id == jokeId){
+        return{saySetup: () => joke.setup,
+               sayPunchLine: () => joke.punchLine
+              };
+   }
+    throw new Error (`No jokes found id: ${jokeId}`);
+  }).catch(error => {
+    throw (error instanceof TypeError)? new Error (`No jokes at url: ${apiUrl}`):error
+  })
+};
