bf314d67eab37c45f1be0730f493c5d828b5c24b
FerLanzaC <88668300+FerLanzaC@users.noreply.github.com>
2022-02-09 23:27:39 -0600

    IMPROVEMENT
    It has some changes because I deleted the .catch. Now , the function checks first if the url is the correct and continue with the next step.

--- method.js
+++ method.js
@@ -1,14 +1,20 @@
 async function sayJoke(apiUrl, jokeId) {
   const response = await fetch(apiUrl)
   const json = await response.json()
-  for(let joke of json.jokes){
-      if (joke.id == jokeId){
-        return {saySetup: () => joke.setup,
-               sayPunchLine: () => joke.punchLine
-              };
-   }
-    throw new Error (`No jokes found id: ${jokeId}`);
-  }).catch(error => {
-    throw (error instanceof TypeError) ? new Error(`No jokes at url: ${apiUrl}`) : error
-  })
+  const jokesChristmas = await json.jokes;
+  
+  if (!jokesChristmas) {
+      throw new Error(`No jokes at url: ${apiUrl}`);
+  } else {
+    for(let joke of json.jokes){
+        if (joke.id == jokeId){
+          return {saySetup: () => joke.setup,
+                 sayPunchLine: () => joke.punchLine
+                };
+     }
+      throw new Error (`No jokes found id: ${jokeId}`);
+    }
+  }
 };
+
+   
