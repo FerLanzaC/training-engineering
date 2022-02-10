6fe1b747df7d107823ba5799e5b37112c7d9a8f7
FerLanzaC <88668300+FerLanzaC@users.noreply.github.com>
2022-02-09 23:28:57 -0600

    Update method.js

--- method.js
+++ method.js
@@ -2,12 +2,11 @@ async function sayJoke(apiUrl, jokeId) {
   const response = await fetch(apiUrl)
   const json = await response.json()
   const jokesChristmas = await json.jokes;
-  
   if (!jokesChristmas) {
       throw new Error(`No jokes at url: ${apiUrl}`);
   } else {
-    for(let joke of json.jokes){
-        if (joke.id == jokeId){
+    for(let joke of json.jokes) {
+        if (joke.id == jokeId) {
           return {saySetup: () => joke.setup,
                  sayPunchLine: () => joke.punchLine
                 };
