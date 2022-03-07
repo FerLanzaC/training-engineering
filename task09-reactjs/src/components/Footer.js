fc343a077410f4abd28fbaef329da3592e2aee24
FerLanzaC <88668300+FerLanzaC@users.noreply.github.com>
2022-03-07 14:03:21 -0600

    Update Footer.js

--- task09-reactjs/src/components/Footer.js
+++ task09-reactjs/src/components/Footer.js
@@ -1,13 +1,11 @@
 import classes from './Footer.module.css'
-function Footer() {
-    return(
-        <footer className= {classes.footer}>
+const Footer = () => (
+    <footer className= {classes.footer}>
             <div className= {classes.footer__body}>
                 <small className={classes.footer__description}>Desarrollado por Akurey.com Todos los derechos reservados.</small>
                 <u className={classes.footer__bottomBody}>Terminos y Condiciones </u>
             </div>
-        </footer>
-    );  
-}
+    </footer>
+)
 
 export default Footer;
