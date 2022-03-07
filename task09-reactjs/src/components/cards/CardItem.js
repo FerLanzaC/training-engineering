1eabdcc5840849033240766b22f27a03fc7a7337
FerLanzaC <88668300+FerLanzaC@users.noreply.github.com>
2022-03-07 16:03:35 -0600

    Update CardItem.js

--- task09-reactjs/src/components/cards/CardItem.js
+++ task09-reactjs/src/components/cards/CardItem.js
@@ -1,19 +1,19 @@
 import classes from './CardItem.module.css'
 
-function CardItem(props) {
+function CardItem({name,isCertified,description,image,rate,hours}) {
     return(
         <div className= {classes.element}>
             <div className= {classes.element__header}>
-                <h3>{props.name}</h3>
-                <img className= {classes.element__icon} src= {props.isCertified}/>
+                <h3>{name}</h3>
+                <img className= {classes.element__icon} src= {isCertified}/>
             </div>
             <div className= {classes.element__body}>
                 <div className= {classes.element__topBody}>
-                <img className= {classes.element__image} src= {props.image}/>
-                <p className= {classes.element__description}> {props.description}</p>
+                <img className= {classes.element__image} src= {image}/>
+                <p className= {classes.element__description}> {description}</p>
                 </div>
                 <div className= {classes.element__bottomBody}>
-                    <b>Desde: ${props.rate} / {props.hours}</b>
+                    <b>Desde: ${rate} / {hours}</b>
                     <button className= {classes.element__button}>Contratar</button>
                 </div>
             </div>
