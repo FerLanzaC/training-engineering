a4fc8697de16815cebdd71d730c7b92d8f3d0ddd
FerLanzaC <88668300+FerLanzaC@users.noreply.github.com>
2022-03-07 15:28:19 -0600

    Update Searcher.js

--- task09-reactjs/src/components/Searcher.js
+++ task09-reactjs/src/components/Searcher.js
@@ -2,17 +2,15 @@ import classes from './Searcher.module.css'
 import searchLogo from './images/search.png'
 import banner from './images/banner-mobile.png'
 
-function Searcher() {
-    return(
-        <div className= {classes.searcher}>
-            <img src={banner} className= {classes.searcher__image}/>
-            <div className={classes.searcher__title}>Los Mejores agentes de seguridad</div>
-            <div className={classes.searcher__bar}>
-            <input type="text" className= {classes.searcher__input} placeholder=" Buscar por Ubicación"/> 
-            <img src={searchLogo} className={classes.searcher__icon}/>     
-            </div>
+const Searcher = () => (
+    <div className= {classes.searcher}>
+        <img src={banner} className= {classes.searcher__image}/>
+        <div className={classes.searcher__title}>Los Mejores agentes de seguridad</div>
+        <div className={classes.searcher__bar}>
+        <input type="text" className= {classes.searcher__input} placeholder=" Buscar por Ubicación"/> 
+        <img src={searchLogo} className={classes.searcher__icon}/>     
         </div>
-    );  
-}
+    </div>
+)
 
 export default Searcher;
